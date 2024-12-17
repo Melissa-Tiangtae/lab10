dc0f7872e543c5acc76700cb6c42e7f8c9abeb8e
Melissa Tiangtae <melissa_tiangate@cmu.ac.th>
2024-12-17 15:44:31 +0700

    lab10_3_completed

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,30 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include<cmath>
+#include<fstream>
+using namespace std;
+
+int main(){
+    int count = 0;
+    float deviation,sum=0,sum_sq=0,mean;
+    ifstream source;
+    ofstream dest;
+    string text;
+    source.open ("score.txt");
+    dest.open ("lab10_3.cpp");
+
+    while(getline(source,text)){
+
+        sum_sq += pow(stof(text),2);
+        sum += stof(text);
+        count ++;
+    }
+
+    mean = sum/count;
+   deviation = pow((((1.0/count)*sum_sq)-pow(mean,2)),0.5);
+
+    cout << "Number of data = " << count <<endl;
+    cout << setprecision(3);
+    cout << "Mean = " << mean << endl;
+   cout << "Standard deviation = " << deviation ;
+}
