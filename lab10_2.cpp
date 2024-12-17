8880f2e1cf7f2164d651dc6d0ccf3771ad278d2a
Melissa Tiangtae <melissa_tiangate@cmu.ac.th>
2024-12-17 15:20:38 +0700

    lab10_2_completed

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -6,10 +6,21 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
 	
 	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
+	string text;
+	dest << "-------------------- BOOM ---------------------"<<endl;
+	
+	while(getline(source, text)){
+
+		dest << text << endl;
+
+	}
+
+	dest << "-------------------- HA!! ---------------------";
+	
 	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
 
     source.close();
